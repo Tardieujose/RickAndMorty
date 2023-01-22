028d9df1673e8e2daae5f80c334bf60dacc74186
Tardieujose <tardieujose@gmail.com>
2023-01-22 20:36:30 -0300

    Hasta aca voy

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return(
+        <div>
+            <h1>Esta es la App de Chuchino</h1>
+            <h2>Vamo que se puede</h2>
+        </div>
+    )
+}
+
+export default About;
